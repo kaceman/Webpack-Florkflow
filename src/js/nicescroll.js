cb565eb40de4288b237b910fa3613165d87a1e00
Kacem <elamrikacem@gmail.com>
2019-07-05 09:55:55 +0100

    fixed horizontal scroll tags listing on the footer

--- src/js/nicescroll.js
+++ src/js/nicescroll.js
@@ -3,14 +3,17 @@ import niceScroll from "nicescroll";
 
 export default class Nicescroll {
     constructor() {
-        this.init()
+        // this.init()
     }
 
     init() {
         $('.doc_footer--tags').niceScroll({
             autohidemode: "hidden",
-            bouncescroll: true,
-            hwacceleration: true
+            // bouncescroll: true,
+            // hwacceleration: true,
+            touchbehavior: true,
+            cursordragontouch: true,
+            emulatetouch: true
         });
     }
 }
