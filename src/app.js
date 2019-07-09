d0ae6ecd96ec074ea5eb5dbc6d8c7689a0cbf168
Kacem <elamrikacem@gmail.com>
2019-07-09 18:10:47 +0100

    article details

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import './scss/main.scss';
 import Index from './js/index'
 import Nicescroll from './js/nicescroll'
 import CountrySwitcher from './js/country-switcher'
+import VideoPopup from "./js/video-popup";
 
 const index = new Index();
 const nicescroll = new Nicescroll();
 const country_switcher = new CountrySwitcher();
+const videoPopup = new VideoPopup();
